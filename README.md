13cd3ffdaedfbc3a82db587e1e27dfcdde00372c
Gileno Filho <gascf.cin@gmail.com>
2015-07-31 23:50:30 -0300

    updating setup.py, readme and events resources

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# tapioca-github
-Github API wrapper using tapioca
+# Tapioca Github
+
+## Instalation
+```
+pip install tapioca-github
+```
+
+## Documentation
+``` python
+from tapioca_github import Github
+
+api = Github(access_token='{access-token}')
+```
+
+No more documentation needed.
+
+- Learn how Tapioca works [here](http://tapioca-wrapper.readthedocs.org/en/latest/quickstart/)
+- Explore this package using iPython
+- Have fun!
--- tapioca_github/resource_mapping.py
+++ tapioca_github/resource_mapping.py
@@ -38,4 +38,44 @@ RESOURCE_MAPPING = {
         'resource': 'gists/{id}/forks',
         'doc': 'https://developer.github.com/v3/gists/#fork-a-gist'
     },
+    'events_public': {
+        'resource': 'events',
+        'doc': 'https://developer.github.com/v3/activity/events/#list-public-events',
+    },
+    'events_repository': {
+        'resource': 'repos/{owner}/{repo}/events',
+        'doc': 'https://developer.github.com/v3/activity/events/#list-repository-events'
+    },
+    'events_issues': {
+        'resource': 'repos/{owner}/{repo}/issues/events',
+        'doc': 'https://developer.github.com/v3/activity/events/#list-issue-events-for-a-repository'
+    },
+    'events_network': {
+        'resource': 'networks/{owner}/{repo}/events',
+        'doc': 'https://developer.github.com/v3/activity/events/#list-public-events-for-a-network-of-repositories'
+    },
+    'events_organization': {
+        'resource': 'orgs/{org}/events',
+        'doc': 'https://developer.github.com/v3/activity/events/#list-public-events-for-an-organization'
+    },
+    'events_user_received': {
+        'resource': 'users/{username}/received_events',
+        'doc': 'https://developer.github.com/v3/activity/events/#list-events-that-a-user-has-received'
+    },
+    'events_user_received_public': {
+        'resource': 'users/{username}/received_events/public',
+        'doc': 'https://developer.github.com/v3/activity/events/#list-public-events-that-a-user-has-received'
+    },
+    'events_user': {
+        'resource': 'users/{username}/events',
+        'doc': 'https://developer.github.com/v3/activity/events/#list-events-performed-by-a-user'
+    },
+    'events_user_public': {
+        'resource': 'users/{username}/events/public',
+        'https://developer.github.com/v3/activity/events/#list-public-events-performed-by-a-user'
+    },
+    'events_user_organization': {
+        'resource': 'users/{username}/events/orgs/{org}',
+        'doc': 'https://developer.github.com/v3/activity/events/#list-events-for-an-organization'
+    }
 }
