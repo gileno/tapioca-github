2794c47e81db2557982bc09d302544d3bcddd976
Filipe Ximenes <filipeximenes@gmail.com>
2016-03-25 15:33:15 -0300

    updating classifiers

--- setup.py
+++ setup.py
@@ -60,12 +60,12 @@ setup(
     zip_safe=False,
     keywords='tapioca-github',
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
     ],
     test_suite='tests',
 )
